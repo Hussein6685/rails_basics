c5506bdfbb09aa2d7c179f09d274c875b5b03a5c
Hussein6685 <113841959+Hussein6685@users.noreply.github.com>
2025-03-16 13:27:36 +0300

    Callback functions in controllers

--- app/controllers/date_controller.rb
+++ app/controllers/date_controller.rb
@@ -1,4 +1,6 @@
 class DateController < ApplicationController
+  before_action :check_name_param, only: [:say_welcome]
+  after_action :finished_processing_request
   def index
   end
 
@@ -11,5 +13,14 @@ class DateController < ApplicationController
   def say_welcome
     render plain: "Welcome #{params[:name]}"
     # @name = params[:name]
+  end
+
+  def check_name_param
+    puts "checking the name #{params[:name]} ==============="
+  end
+
+  def finished_processing_request
+    # render plain: "Finished processing request"
+    puts "Finished processing request ======================"
   end
 end
